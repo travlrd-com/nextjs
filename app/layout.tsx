cb7927fe62f05be06e59628b4559daed91f87c1b
Viktor Tar <viktortar98@gmail.com>
2024-03-04 13:14:08 +0100

    add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`grid overflow-y-scroll min-h-screen ${inter.className}`}>{children}</body>
+      <body className={`grid overflow-y-scroll min-h-screen ${inter.className}`}>
+        {children}
+        <MetaPixel />
+      </body>
     </html>
+  );
+}
+
+
+
+//NOTE - Got the pixel code from here:
+// https://business.facebook.com/events_manager2/list/dataset/DATASET_ID/overview
+function MetaPixel() {
+  const DATASET_ID = '---';
+
+  return (
+    <Script
+      id="fb-pixel"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${DATASET_ID}');
+          fbq('track', 'PageView');
+        `}
+      }
+    />
   );
 }

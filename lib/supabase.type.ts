a05b319f52dfe14c0c8bece0492ddb97f86c9746
Viktor Tar <viktortar98@gmail.com>
2024-04-19 12:12:40 +0200

    Remove unused supabase.type.ts file

--- lib/supabase.type.ts
+++
@@ -1,4 +0,0 @@
-import { Database } from "./database.types";
-
-export type DB<Type extends "Insert" | "Relationships" | "Row" | "Update", TableName extends keyof Database["public"]["Tables"]> =
-  Database["public"]["Tables"][TableName][Type];

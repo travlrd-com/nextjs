6d835187080e4b0cde7abc6fd81ff581a4c572cf
Viktor Tar <viktortar98@gmail.com>
2024-08-26 09:36:01 +0200

    update stripe version

--- lib/stripe.server.ts
+++ lib/stripe.server.ts
@@ -3,5 +3,5 @@ import Stripe from "stripe";
 import { env } from "./env.server";
 
 export function createStripeClient() {
-  return new Stripe(env.STRIPE_API_KEY, { apiVersion: "2023-10-16", httpClient: Stripe.createFetchHttpClient() });
+  return new Stripe(env.STRIPE_API_KEY, { apiVersion: "2024-04-10", httpClient: Stripe.createFetchHttpClient() });
 }
